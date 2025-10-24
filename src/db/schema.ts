c16f7999d1d34094070f5f080f9addee41682184
Suraj Pulami <surajpulami8@gmail.com>
2025-10-24 15:36:20 +0545

    fix: timestamp precison conflict between JS & postgres

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -2,10 +2,26 @@ import { pgTable, text, timestamp } from 'drizzle-orm/pg-core';
 import { createInsertSchema, createSelectSchema } from 'drizzle-zod';
 import { cuid2 } from 'drizzle-cuid2/postgres';
 
+export function nowMs() {
+  const d = new Date();
+  d.setUTCMilliseconds(Math.floor(d.getUTCMilliseconds())); // ensure 3ms precision
+  return d;
+}
+
+export function timestampMs(name: string, update = false) {
+  if (update) return timestamp(name, { mode: 'date' })
+    .notNull()
+    .default(nowMs())
+    .$onUpdate(() => nowMs());
+  return timestamp(name, { mode: 'date' })
+    .notNull()
+    .default(nowMs())
+}
+
 export const example = pgTable('example', {
   id: cuid2('id').defaultRandom().primaryKey(),
   name: text('name').notNull(),
-  createdAt: timestamp('createdAt', { mode: 'date' }).defaultNow().notNull(),
+  createdAt: timestampMs('createdAt'), // NOTE: this must be used for proper cursor pagination
 });
 
 export const selectExampleSchema = createSelectSchema(example);
